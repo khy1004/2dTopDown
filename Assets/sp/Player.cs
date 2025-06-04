d01832b1cb16a209d7129372735a883ba08684a7
khy1004 <khyseog08@gmail.com>
2025-06-04 16:16:37 +0900

    [feat] 11강 과제 Scriptable Object 및 Top-Down 게임 제작

---
+++ Assets/sp/Player.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Player : MonoBehaviour
+{
+    float moveSpeed = 2f;
+    public float score;
+
+    public TextMeshProUGUI ScoreText;
+
+    [SerializeField] Sprite spriteUp;
+    [SerializeField] Sprite spriteDown;
+    [SerializeField] Sprite spriteLeft;
+    [SerializeField] Sprite spriteRight;
+
+    Rigidbody2D rb;
+    SpriteRenderer sR;
+
+    Vector2 input;
+    Vector2 velocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sR = GetComponent<SpriteRenderer>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+
+  
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        velocity = input.normalized * moveSpeed;
+
+        if(input.sqrMagnitude > .01f)
+        {
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+            {
+                if (input.x > 0)
+                    sR.sprite = spriteRight;
+                else if (input.x < 0)
+                    sR.sprite = spriteLeft;
+            }
+        }
+        else
+        {
+            if (input.y > 0)
+                sR.sprite = spriteUp;
+            else
+                sR.sprite = spriteDown;
+        }
+
+        ScoreText.text = "Score" + score.ToString();
+    }
+
+   
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Item"))
+        {
+            score += collision.GetComponent<ItemObject>().GetPoint();
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
